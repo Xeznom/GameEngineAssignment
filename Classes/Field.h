eb14eee88d6df5a88e9e2ee7151d21ffc9e2b1e0
Xeznom <Xeznom@gmail.com>
2015-07-16 13:08:12 +0800

    null pointer fix and added a new png for laser

--- Classes/Field.h
+++ Classes/Field.h
@@ -17,12 +17,15 @@ enum M_TILE_TYPES {
 	M_TRAP,
 	M_ENEMY,
 	M_BUTTON,
+	M_MOBILESPIKE,
+	M_LASER,
+	M_COIN,
 	M_TOTAL
 	
 } ;
 
 static const char* const g_scTileFileName[M_TOTAL] = {
-	"empty.png", "white.png", "empty.png", "spike.png","empty.png","empty.png"
+	"empty.png", "white.png", "empty.png", "spike.png","empty.png","empty.png","empty.png", "yellow.png", "door.png"
 };
 
 class CField : public CEntity
